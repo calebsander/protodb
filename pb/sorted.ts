12d586a7b78646b4c38544583f245a3eceedef11
Caleb Sander <caleb.sander@gmail.com>
2019-06-01 15:05:09 -0700

    Use CommonJS imports for sorted maps

--- pb/sorted.ts
+++ pb/sorted.ts
@@ -1,5 +1,5 @@
-import path from 'path'
-import protobuf from 'protobufjs'
+import path = require('path')
+import protobuf = require('protobufjs')
 import {Type} from './common'
 import {Key} from './interface'
 
